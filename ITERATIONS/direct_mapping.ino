94e53807164186bc23a2c97860dd3eb9b2665603
Ezra Mabatu <ezramabatu@gmail.com>
2025-12-30 18:15:03 +0300

    Add Bluepad32 controller support for motor control
    Implement controller handling and motor control logic.

---
+++ ITERATIONS/direct_mapping.ino
@@ -0,0 +1,188 @@
+#include <Bluepad32.h>
+
+#define rightWheels 33
+#define leftWheels 32
+
+#define RightWheelsForward 22
+#define RightWheelsReverse 19
+#define LeftWheelsForward 23
+#define LeftWheelsReverse 18
+
+#define NEUTRAL_POINT 4
+#define DEAD_ZONE 4
+#define MAX_SPEED 1023
+#define MIN_SPEED 200
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+void onConnectedController(ControllerPtr ctl) {
+  bool foundEmptySlot = false;
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+      ControllerProperties properties = ctl->getProperties();
+      Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                    properties.product_id);
+      myControllers[i] = ctl;
+      foundEmptySlot = true;
+      break;
+    }
+  }
+  if (!foundEmptySlot) {
+    Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+  }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  bool foundController = false;
+
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+      myControllers[i] = nullptr;
+      foundController = true;
+      break;
+    }
+  }
+
+  if (!foundController) {
+    Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+  }
+}
+
+void processGamepad(ControllerPtr ctl) {
+  //All motors off -> direction control.
+  digitalWrite(RightWheelsForward, LOW);
+  digitalWrite(RightWheelsReverse, LOW);
+  digitalWrite(LeftWheelsForward, LOW);
+  digitalWrite(LeftWheelsReverse, LOW);
+
+  analogWrite(leftWheels, 0);
+  analogWrite(rightWheels, 0);
+
+  int LY = ctl->axisY();
+  int RX = ctl->axisRX();
+  int L2 = ctl->brake();
+  int R2 = ctl->throttle();
+  int buttons = ctl->buttons();
+
+  //Controls for left wheels.
+  if (LY < NEUTRAL_POINT - DEAD_ZONE) {
+    //Set direction.
+    digitalWrite(RightWheelsForward, HIGH);
+    digitalWrite(RightWheelsReverse, LOW);
+    digitalWrite(LeftWheelsForward, HIGH);
+    digitalWrite(LeftWheelsReverse, LOW);
+
+
+    // int dutyCycle = map(LY, (NEUTRAL_POINT - DEAD_ZONE - 1), -508, MIN_SPEED, MAX_SPEED);
+    analogWrite(rightWheels, -1 * (LY - 4) * 2);
+    analogWrite(leftWheels, (-1 * LY - 4) * 2);
+  }
+
+  else if (LY > NEUTRAL_POINT + DEAD_ZONE) {
+    //Set direction.
+    digitalWrite(RightWheelsForward, LOW);
+    digitalWrite(RightWheelsReverse, HIGH);
+    digitalWrite(LeftWheelsForward, LOW);
+    digitalWrite(LeftWheelsReverse, HIGH);
+
+    //Set speed.
+    // int dutyCycle = map(LY, (NEUTRAL_POINT + DEAD_ZONE + 1), 512, MIN_SPEED, MAX_SPEED);
+    analogWrite(leftWheels, (LY - 4) * 2);
+    analogWrite(rightWheels, (LY - 4) * 2);
+  }
+
+  if (RX > NEUTRAL_POINT + DEAD_ZONE) {
+    //Set direction.
+    digitalWrite(RightWheelsForward, HIGH);
+    digitalWrite(RightWheelsReverse, LOW);
+    digitalWrite(LeftWheelsForward, LOW);
+    digitalWrite(LeftWheelsReverse, HIGH);
+
+    //Set speed.
+    // int dutyCycle = map(RX, (NEUTRAL_POINT + DEAD_ZONE + 1), 512, MIN_SPEED, MAX_SPEED);
+    analogWrite(rightWheels, (RX - 4) * 2);
+    analogWrite(leftWheels, (RX - 4) * 2);
+  }
+
+  else if (RX < NEUTRAL_POINT - DEAD_ZONE) {
+    //Set direction.
+    digitalWrite(RightWheelsForward, LOW);
+    digitalWrite(RightWheelsReverse, HIGH);
+    digitalWrite(LeftWheelsForward, HIGH);
+    digitalWrite(LeftWheelsReverse, LOW);
+
+
+    //Set speed.
+    //int dutyCycle = map(RX, (NEUTRAL_POINT - DEAD_ZONE - 1), -508, MIN_SPEED, MAX_SPEED);
+    analogWrite(leftWheels, -1 * (RX - 4) * 2);
+    analogWrite(rightWheels, -1 * (RX - 4) * 2);
+  }
+  if (L2 > 4) {
+    digitalWrite(RightWheelsForward, LOW);
+    digitalWrite(RightWheelsReverse, HIGH);
+    digitalWrite(LeftWheelsForward, LOW);
+    digitalWrite(LeftWheelsReverse, HIGH);
+
+    int dutyCycle = map(L2, 0, 1023, MIN_SPEED, MAX_SPEED);
+    analogWrite(rightWheels, dutyCycle);
+    analogWrite(leftWheels, dutyCycle);
+  }
+  if (R2 > 4) {
+    digitalWrite(RightWheelsForward, HIGH);
+    digitalWrite(RightWheelsReverse, LOW);
+    digitalWrite(LeftWheelsForward, HIGH);
+    digitalWrite(LeftWheelsReverse, LOW);
+
+    int dutyCycle = map(R2, 0, 1023, MIN_SPEED, MAX_SPEED);
+    analogWrite(rightWheels, dutyCycle);
+    analogWrite(leftWheels, dutyCycle);
+  }
+}
+
+void processControllers() {
+  for (auto myController : myControllers) {
+    if (myController && myController->isConnected() && myController->hasData()) {
+      if (myController->isGamepad()) {
+        processGamepad(myController);
+      } else {
+        Serial.println("Unsupported controller");
+      }
+    }
+  }
+}
+
+void setup() {
+  //direction control for motors, for use with L298 motor driver
+  pinMode(RightWheelsForward, OUTPUT);
+  pinMode(RightWheelsReverse, OUTPUT);
+  pinMode(LeftWheelsForward, OUTPUT);
+  pinMode(LeftWheelsReverse, OUTPUT);
+
+  //PWM pins to control speed of wheels
+  pinMode(rightWheels, OUTPUT);
+
+  pinMode(leftWheels, OUTPUT);
+
+
+  analogWriteFrequency(25000);
+  analogWriteResolution(10);
+
+  Serial.begin(115200);
+  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+  BP32.forgetBluetoothKeys();
+  BP32.enableVirtualDevice(false);
+}
+
+void loop() {
+  bool dataUpdated = BP32.update();
+  if (dataUpdated) {
+    processControllers();
+  }
+}
